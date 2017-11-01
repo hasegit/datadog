428110660643394e79192eb1b2eab4f38f0f0bbe
hasegit <hasegit@gmail.com>
2017-11-02 00:21:14 +0900

    commit test

---
+++ custom_log_parser.py
@@ -0,0 +1,44 @@
+#!/opt/datadog-agent/embedded/bin/python
+
+import time
+import yaml
+import re
+import time
+from datetime import datetime
+
+def parse_log(logger,line):
+    f = open('/opt/datadog-agent/agent/checks.d/log.yaml')
+    data = yaml.load(f)
+    f.close()
+
+    for c in data['config']:
+        if re.match(c['check_regex'],line) and not re.match(c['ignore_regex'],line):
+            now = datetime.now()
+            date = int(time.mktime(now.timetuple()))
+            logger_event = {
+                'msg_title': c['title'],
+                'timestamp': date,
+                'msg_text': line,
+                'alert_type': 'error',
+                'event_type': 'dada',
+                'aggregation_key': 'popo',
+                'tags': ['id:' + c['id'] ],
+                'priority': 'normal'
+            }
+
+            return logger_event
+    return 'naiyo!'
+
+def test():
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    line = 'something error'
+    print parse_log(logging,line)
+
+    line = 'poipoi error'
+    print parse_log(logging,line)
+
+
+if __name__ == '__main__':
+    test()
